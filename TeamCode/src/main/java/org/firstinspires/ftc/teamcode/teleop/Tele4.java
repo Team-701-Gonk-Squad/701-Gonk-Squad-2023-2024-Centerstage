ae14098fb2908e10cb1fa59b9156092a66ba9a5a
Ronan <spacechickenrobot@gmail.com>
2024-01-09 15:20:10 -0500

    Teleop works in theory, need to add limit on box still

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Tele4.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Tele4.java
@@ -23,24 +23,45 @@ import java.util.Timer;
 
 public class Tele4 extends LinearOpMode {
     Hardware hardware;
+    double position;
 
     public void runOpMode() {
 
         hardware = new Hardware(hardwareMap);
 
+        position = 0;
+
         waitForStart();
 
         while (opModeIsActive()) {
+//            if (gamepad1.a) {
+//                hardware.door.setPosition(0);
+//                hardware.boxRotation.setPosition(1);
+//            } else if (gamepad1.b) {
+//                hardware.door.setPosition(0.25);
+//                hardware.boxRotation.setPosition(0);
+//            }
 
-            if (gamepad2.a) {
-                hardware.door.setPosition(0);
-                hardware.boxRotation.setPosition(0);
-            }
+//            if (gamepad1.left_trigger > 0) {
+                position = gamepad1.left_trigger;
+//            }
 
-            if (gamepad1.b) {
-                hardware.door.setPosition(0.25);
-                hardware.boxRotation.setPosition(1);
-            }
+//            if (gamepad1.a) {
+//                position = 0.25;
+//                telemetry.addData("A Pressed", 1);
+//            } else if (gamepad1.b) {
+//                position = 0;
+//                telemetry.addData("B Pressed", 1);
+//            } else {
+//                telemetry.addData("A Pressed", 0);
+//                telemetry.addData("B Pressed", 0);
+//            }
+
+            hardware.boxRotation.setPosition(position);
+//            hardware.door.setPosition(position);
+
+            telemetry.addData("Position", position);
+            telemetry.update();
         }
     }
 }
