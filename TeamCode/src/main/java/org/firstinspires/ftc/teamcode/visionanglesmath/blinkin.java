cbb60d36077037b611c69e545e21cf0fdc442036
Ronan <spacechickenrobot@gmail.com>
2024-02-05 17:17:40 -0500

    blinkin.java - blinkin testing code, will run rainbow_red for 10 seconds

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/visionanglesmath/blinkin.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/visionanglesmath/blinkin.java
@@ -1,7 +1,14 @@
 package org.firstinspires.ftc.teamcode.visionanglesmath;
 
+import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp
 public class blinkin extends LinearOpMode {
-    public void runOpMode() {}
+    public RevBlinkinLedDriver signal;
+    public void runOpMode() {
+        signal = hardwareMap.get(RevBlinkinLedDriver.class, "blinkin");
+        signal.setPattern(RevBlinkinLedDriver.BlinkinPattern.RAINBOW_LAVA_PALETTE);
+        sleep(10000);
+    }
 }
